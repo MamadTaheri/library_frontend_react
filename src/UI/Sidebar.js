d4ddfd319d1ac8072ca70c870c1f647873420486
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-10-20 23:12:10 +0330

    create Sidebar component

---
+++ src/UI/Sidebar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <>
+      <h4>داشبورد</h4>
+      <br />
+      <hr className="bg-white" />
+      <ul>
+        <li>کاربران</li>
+        <li>کتابها</li>
+      </ul>
+    </>
+  );
+};
+
+export default Sidebar;
